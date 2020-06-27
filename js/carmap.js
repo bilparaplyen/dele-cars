fe91708151bca4403170d2cd789464eca0f85e63
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2020-06-27 14:19:56 +0200

    Fix syntax error. Also implement hashcode as normal function

--- js/carmap.js
+++ js/carmap.js
@@ -1,24 +1,18 @@
-Object.defineProperty(String.prototype, 'hashCode', {
-        value: function() {
-        var hash = 0, i, chr;
-        for (i = 0; i < this.length; i++) {
-            chr   = this.charCodeAt(i);
-            hash  = ((hash << 5) - hash) + chr;
-            hash |= 0; // Convert to 32bit integer
-        }
-        return hash;
+function hashCode(str) {
+    var hash = 0, i, chr;
+    for (i = 0; i < str.length; i++) {
+        chr = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
     }
-});
-
-funtion abctest(){
-console.log("This is a test");
+    return hash;
 }
+
 //Create a map with all the cars
 function createMap(token) {
     var mymap = L.map('mapid').setView([60.39078164, 5.32055452], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery ©
-    <a href="https://www.mapbox.com/">Mapbox</a>',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox/streets-v11',
         tileSize: 512,
@@ -31,7 +25,7 @@ function createMap(token) {
     };
     for (let index = 0; index < data.cars.length; index++) {
         const car = data.cars[index];
-        const key = JSON.stringify(car.location.geojson).hashCode();
+        const key = hashCode(JSON.stringify(car.location.geojson));
         var feature = L.geoJSON(car.location.geojson, {style:style});
         var point = feature.getBounds().getCenter();
         if(!(key in features)) {
